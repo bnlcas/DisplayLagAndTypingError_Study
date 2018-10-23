d4776e658e056afd65b6b688c2c041bcdb6d045b
Benjamin Lucas <benjaminlucas@Benjamins-MacBook-Pro.local>
2018-10-22 21:27:57 -0700

    added jquery to make this work on multiple browsers and accept a backspace events

--- latency_text_input_simulation.js
+++ latency_text_input_simulation.js
@@ -1,6 +1,7 @@
 	var paragraph;// = document.getElementById("p");
 	var entryField;
 	var added_text = "";
+	var initial_output_message;
 
 	var frameTime = 100;
 	var latency = 50;
@@ -19,23 +20,62 @@
 		setTimeout(() => entryField.value = "",1);
   }
 
+	$(document).ready(function () {
+	    $("#textEntry").keydown(function(e) {
+				var keynum = e.keyCode;
+
+				if(keynum == 13){
+					paragraph.innerHTML = initial_output_message;
+				}
+			else {
+				//Delete on Backspace
+				if(keynum == 8)
+				{
+					if(added_text.length > 0)
+					{
+						added_text = added_text.slice(0,-1);
+					}
+					else
+					{
+						var current_text = paragraph.innerHTML;
+						if(current_text.length > initial_output_message.length)
+						{
+							paragraph.innerHTML = current_text.slice(0,-1);
+						}
+					}
+				}
+				else
+				{
+						added_text += String.fromCharCode(keynum).toLowerCase();
+				}
+				setTimeout(() => entryField.value = "",1);
+
+			}
+	    });
+	});
+
+
+
+
+
   function start()
   {
   	paragraph = document.getElementById("p");
 		entryField = document.getElementById("textEntry");
+		initial_output_message = paragraph.innerHTML;
   }
 
   function drawWLatency()
   {
-	var current_text = added_text;
-	setTimeout(() => draw(current_text), latency);
-	added_text = "";
+		var current_text = added_text;
+		setTimeout(() => draw(current_text), latency);
+		added_text = "";
   }
 
 
   function draw(inputText)
   {
-	paragraph.innerHTML += inputText;
+		paragraph.innerHTML += inputText;
   }
 
   function editFrameRate()
